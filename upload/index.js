974173f959a27284b323375c44a8802de03fbe5e
Maciej Winnicki <maciej.winnicki@serverless.com>
2016-10-20 13:55:58 +0200

    add dropzone

--- upload/index.js
+++ upload/index.js
@@ -2,16 +2,37 @@ const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
 
 module.exports.upload = function (event, context, callback) {
-  const params = {Bucket: 'sconf-workshop-uploads', Key: 'images', Expires: 60} // expires in 60 seconds
+  const params = {
+    Bucket: 'sconf-workshop-uploads',
+    Key: `image${Date.now()}.jpeg`,
+    ContentType: 'image/jpeg',
+    ACL: 'public-read',
+    Expires: 300 // expires in 5 minutes
+  }
   const url = s3.getSignedUrl('putObject', params)
 
   const html = `
   <html>
+    <head>
+      <script src="https://cdn.rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
+      <script type="text/javascript">
+        Dropzone.options.myAwesomeDropzone = {
+          method: 'put',
+          acceptedFiles: 'image/jpeg',
+          sending: function(file, xhr) {
+            xhr.setRequestHeader('Content-Type', file.type)
+            xhr.setRequestHeader('x-amz-acl', 'public-read')
+            var _send = xhr.send
+            xhr.send = function() {
+              _send.call(xhr, file)
+            }
+          }
+        };
+      </script>
+    </head>
     <body>
       <h1>Upload you images</h1>
-      <p>
-        Link to S3: <a href="${url}">${url}</a>
-      </p>
+      <form action="${url}" class="dropzone" id="my-awesome-dropzone"></form>
     </body>
   </html>`
 
